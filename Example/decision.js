34277982a985ce681c001a1c7be474d56bd363e7
Anhvmz <Anhvmz@gmail.com>
2017-12-29 20:58:06 +0700

    fix Example

--- Example/decision.js
+++ Example/decision.js
@@ -4,7 +4,7 @@
 //   { yours: true, theirs: false },
 // ]
 
-function countTrue(array) {
+function countTheirsTrue(array) {
   var count = array.reduce((currentCount, number) => {
     number.theirs ? currentCount++ : {};
     return currentCount;
@@ -12,14 +12,27 @@ function countTrue(array) {
   return count;
 }
 
+function countMeTrue(array) {
+  var count = array.reduce((currentCount, number) => {
+    number.yours ? currentCount++ : {};
+    return currentCount;
+  }, 0);
+  return count;
+}
+
+function checkMe(history) {
+  //return Math.random() < 0.5 ? true : false;
+  const myRate = countMeTrue(history) / history.length;
+  return myRate >= 0.5 ? false : Math.random() < 0.5 ? true : false;;
+}
 const decide = (history = []) => {
-  const rate = countTrue(history) / history.length;
-  return rate >= 0.5 ? Math.random() < 0.5 ? true : false : false;
+  const rate = countTheirsTrue(history) / history.length;
+  return rate > 0.5 ? checkMe(history) : false;
 }
 // var data = [
 //   { yours: true, theirs: true },
-//   { yours: true, theirs: false },
-//   { yours: true, theirs: true },
+//   { yours: false, theirs: false },
+//   { yours: false, theirs: true },
 // ]
 // console.log(decide(data));
 module.exports = decide;
