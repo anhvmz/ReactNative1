cb22716a49e57f5e57f373bb682cc91fe0fce427
Anhvmz <Anhvmz@gmail.com>
2017-12-29 20:27:19 +0700

    fix Example

--- Example/decision.js
+++ Example/decision.js
@@ -3,8 +3,20 @@
 //   { yours: true, theirs: true },
 //   { yours: true, theirs: false },
 // ]
-const decide = (history = []) => {
-  return false; // Your code here
+
+function countTrue(array) {
+  var count = array.reduce((currentCount, number) => {
+    number ? currentCount++ : {};
+    return currentCount;
+  }, 0);
+  return count;
 }
 
+const decide = (history = []) => {
+  const rate = countTrue(history) / history.length;
+  console.log(rate);
+  return rate >= 0.5 ? true : false;
+}
+// var data = [true, false, false, false, true];
+// console.log(decide(data));
 module.exports = decide;
